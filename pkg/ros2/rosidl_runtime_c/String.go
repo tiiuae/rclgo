c6c011015dda1a6bfeaf8190525f44a9db08291a
Lassi Laiho <lassi.laiho@solita.fi>
2021-03-26 22:07:33 +0000

    Complex String/Array serdes contribution from Lassi Laiho.

--- pkg/ros2/rosidl_runtime_c/String.go
+++ pkg/ros2/rosidl_runtime_c/String.go
@@ -10,6 +10,7 @@ package rosidl_runtime_c
 import "C"
 import (
 	"fmt"
+	"strings"
 	"unsafe"
 )
 
@@ -25,4 +26,35 @@ func (t *String) PrepareMemory() unsafe.Pointer {
 }
 func (t *String) ReleaseMemory(pointer_to_free unsafe.Pointer) {
 	fmt.Printf("rosidl_runtime_c.ReleaseMemory() called. This is never meant to be directly addressed as a stand-alone data object in the ROS2 messaging bus.")
+}
+func (t *String) AsCStruct() unsafe.Pointer { // rosidl_runtime_c__String__assignn() does something like this, but to call it we still need to make a C string and free it.
+	mem := (*C.rosidl_runtime_c__String)(C.malloc(C.sizeof_struct_rosidl_runtime_c__String)) //TODO add this to template generator
+
+	mem.data = (*C.char)(C.malloc((C.size_t)(C.sizeof_char * uintptr(len(*t)+1))))
+
+	for i := 0; i < len(*t); i++ {
+		t.setDataCArrayIndex(mem, i, (*t)[i])
+	}
+	t.setDataCArrayIndex(mem, len(*t), '\x00')
+	mem.size = C.size_t(len(*t))
+	mem.capacity = C.size_t(len(*t) + 1)
+	return unsafe.Pointer(mem)
+}
+func (t *String) setDataCArrayIndex(mem *C.rosidl_runtime_c__String, i int, v byte) {
+	cIdx := (*C.uint8_t)(unsafe.Pointer(
+		uintptr(unsafe.Pointer(mem.data)) + (unsafe.Sizeof(C.uint8_t(0)) * uintptr(i)),
+	))
+	*cIdx = (C.uint8_t)(v)
+}
+func (t *String) AsGoStruct(ros2_message_buffer unsafe.Pointer) {
+	mem := (*C.rosidl_runtime_c__String)(ros2_message_buffer)
+	sb := strings.Builder{}
+	sb.Grow(int(mem.size))
+	for i := 0; i < int(mem.size); i++ {
+		cIdx := (*C.uint8_t)(unsafe.Pointer(
+			uintptr(unsafe.Pointer(mem.data)) + (unsafe.Sizeof(C.uint8_t(0)) * uintptr(i)),
+		))
+		sb.WriteByte(byte(*cIdx))
+	}
+	*t = String(sb.String())
 }

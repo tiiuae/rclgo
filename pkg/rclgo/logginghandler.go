1fd0cf6e3d8d9db9b29de57f06d9c7df24c22dd8
Lassi Laiho <lassi.laiho@solita.fi>
2021-12-22 14:26:16 +0000

    Remove C types from LoggingOutputHandler

--- pkg/rclgo/logginghandler.go
+++ pkg/rclgo/logginghandler.go
@@ -2,17 +2,18 @@ package rclgo
 
 // #include <rcl/logging.h>
 import "C"
+import "unsafe"
 
 // LoggingOutputHandler is the function signature of logging output handling.
 // Backwards compatibility is not guaranteed for this type alias. Use it only if
 // necessary.
 type LoggingOutputHandler = func(
-	location *C.rcutils_log_location_t,
-	severity C.int,
-	name *C.char,
-	timestamp C.rcutils_time_point_value_t,
-	format *C.char,
-	args *C.va_list,
+	location unsafe.Pointer,
+	severity int,
+	name unsafe.Pointer,
+	timestamp int64,
+	format unsafe.Pointer,
+	args unsafe.Pointer,
 )
 
 // GetLoggingOutputHandler returns the current logging output handler.
@@ -38,14 +39,21 @@ func SetLoggingOutputHandler(h LoggingOutputHandler) {
 // which logs messages based on ROS parameters used to initialize the logging
 // system.
 func DefaultLoggingOutputHandler(
-	location *C.rcutils_log_location_t,
-	severity C.int,
-	name *C.char,
-	timestamp C.rcutils_time_point_value_t,
-	format *C.char,
-	args *C.va_list,
+	location unsafe.Pointer,
+	severity int,
+	name unsafe.Pointer,
+	timestamp int64,
+	format unsafe.Pointer,
+	args unsafe.Pointer,
 ) {
-	C.rcl_logging_multiple_output_handler(location, severity, name, timestamp, format, args)
+	C.rcl_logging_multiple_output_handler(
+		(*C.rcutils_log_location_t)(location),
+		C.int(severity),
+		(*C.char)(name),
+		C.rcutils_time_point_value_t(timestamp),
+		(*C.char)(format),
+		(*C.va_list)(args),
+	)
 }
 
 //export loggingOutputHandler
@@ -59,5 +67,12 @@ func loggingOutputHandler(
 ) {
 	loggingMutex.Lock()
 	defer loggingMutex.Unlock()
-	currentLoggingOutputHandler(location, severity, name, timestamp, format, args)
+	currentLoggingOutputHandler(
+		unsafe.Pointer(location),
+		int(severity),
+		unsafe.Pointer(name),
+		int64(timestamp),
+		unsafe.Pointer(format),
+		unsafe.Pointer(args),
+	)
 }
